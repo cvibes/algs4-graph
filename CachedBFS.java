a29b3ba641bb8d4ea2dae817a0fa6f62a7db7db4
Xiaohui Lu <xlu3@bmr013.srl.ford.com>
2015-03-30 18:34:13 -0400

    working but buggy

--- CachedBFS.java
+++ CachedBFS.java
@@ -3,44 +3,60 @@ public class CachedBFS
     private final int V;
     private ST<Integer, Integer>[] st;
     private boolean[] marked;
+    private Digraph dg;
     
     public CachedBFS(Digraph dg) {
         V      = dg.V();
-        st     = (ST<Integer, Integer>[]) new ST[V];
+        this.dg = dg;
         marked = new boolean[V];
-
+        st     = (ST<Integer, Integer>[]) new ST[V];
         for (int i = 0; i < V; i++)
             st[i] = new ST<Integer, Integer>();
-
-        for (int i = 0; i < V; i++)
-            bfs(dg, i);
     }
 
-    public void bfs(Digraph dg, int s) {
-        Queue<Integer> q = new Queue<Integer>();
-        marked[s] = true;
+    public void bfs(int s) {
+        boolean[] visited = new boolean[V];
+        int dist          = 0;
+        Queue<Integer> q  = new Queue<Integer>();
+
+        visited[s] = true;
         q.enqueue(s);
+        q.enqueue(dist);
+
         while (!q.isEmpty()) {
             int v = q.dequeue();
-            st[v].put(v, 0);
+            dist  = q.dequeue();
+            if (st[s].get(v) == null || dist < st[s].get(v))
+                st[s].put(v, dist);
+            PATH:
             for (int w : dg.adj(v))
-                if (!marked[w]) {
-                    marked[w] = true;
-                    st[w].put(v, 1);
+                if (!visited[w]) {
+                    if (marked[w]) {
+                        for (int x : st[w].keys())
+                            if (st[s].get(x) == null
+                                || (dist + st[w].get(x)) < st[s].get(x))
+                                st[s].put(x, dist + st[w].get(x));
+                        break PATH;
+                    }
+                    visited[w] = true;
                     q.enqueue(w);
+                    q.enqueue(dist + 1);
                 }
-                else
-                    for (int n : st[w].keys())
-                        st[v].put(n, st[w].get(n) + 1);
         }
+        marked[s] = true;
     }
 
+    public ST<Integer, Integer> ancestors(int v) {
+        if (!marked[v])
+            bfs(v);
+        return st[v];
+    }
     public String toString() {
         String s = "";
         for (int i = 0; i < V; i++) {
             s = s + "Ancestors of " + i + "\n";
-            for (int j : st[i].keys())
-                s = s + j + " : " + st[i].get(j) + "\n";
+            for (int j : ancestors(i).keys())
+                s = s + j + " : " + ancestors(i).get(j) + "\n";
             s = s + "\n";
         }
         return s;
@@ -50,6 +66,9 @@ public class CachedBFS
         In in = new In(args[0]);
         Digraph dg = new Digraph(in);
         CachedBFS bfs = new CachedBFS(dg);
+        for (int i = 0; i < dg.V(); i++) {
+            bfs.bfs(i);
+        }
         System.out.print(bfs.toString());
     }
 }
--- SAP.java
+++ SAP.java
@@ -1,31 +1,35 @@
 public class SAP {
-    private final int V;
-    private ST<Integer, Integer>[] st;
-    private boolean[]  marked;
+    private CachedBFS cbfs;
 
     public SAP(Digraph G) {
-        V      = G.V();
-        st     = ((ST<Integer, Integer>[]) new ST[V]);
-        marked = new boolean[V];
-
-        for (int i = 0; i < V; i++) {
-            st[i] = new ST<Integer, Integer>();
-            for (int a : G.adj(i)) {
-                st[i].put(a, 1);
-            }
-        }
-    }
-
-    private void bfs(Digraph dg, int s) {
-        
+        cbfs = new CachedBFS(G);
     }
 
     public int length(int v, int w) {
-        return 0;
+        ST<Integer, Integer> aofv = cbfs.ancestors(v);
+        ST<Integer, Integer> aofw = cbfs.ancestors(w);
+        int minDist = -1;
+        for (int x : aofv.keys())
+            if (aofw.get(x) != null
+                && (minDist == -1 || (aofv.get(x) + aofw.get(x)) < minDist))
+                    minDist = aofv.get(x) + aofw.get(x);
+        
+        return minDist;
     }
 
     public int ancestor(int v, int w) {
-        return 0;
+        ST<Integer, Integer> aofv = cbfs.ancestors(v);
+        ST<Integer, Integer> aofw = cbfs.ancestors(w);
+        int minDist = -1;
+        int ancestor = -1;
+        for (int x : aofv.keys()) {
+            if (aofw.get(x) != null
+                && (minDist == -1 || (aofv.get(x) + aofw.get(x)) < minDist)) {
+                    minDist = aofv.get(x) + aofw.get(x);
+                    ancestor = x;
+                }
+        }
+        return ancestor;
     }
 
     public int length(Iterable<Integer> v, Iterable<Integer> w) {
@@ -39,8 +43,14 @@ public class SAP {
     public static void main(String[] args) {
         In in = new In(args[0]);
         Digraph g = new Digraph(in);
-        SAP s = new SAP(g);
-        return;
+        SAP sap = new SAP(g);
+        while (!StdIn.isEmpty()) {
+            int v = StdIn.readInt();
+            int w = StdIn.readInt();
+            int length = sap.length(v, w);
+            int ancestor = sap.ancestor(v, w);
+            StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
+        }
     }
 
 }
